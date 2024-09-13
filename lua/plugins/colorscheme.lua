6b66e9472791673d678c0421b9c06360eff2f7e7
Rafe <rsymonds@umich.edu>
2024-09-12 22:31:19 -0400

    New potential theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -14,3 +14,36 @@ return {
         vim.cmd("colorscheme flow")
     end,
 }
+-- return {
+--     -- Other plugins ...
+--
+--     "lancewilhelm/horizon-extended.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         require("horizon-extended").setup({
+--             style = "neo",
+--             transparent = false,
+--             terminal_colors = true,
+--             enable_italics = false,
+--             show_end_of_buffer = false,
+--             underline = false,
+--             undercurl = true,
+--             styles = {
+--                 booleans = { italic = false, bold = false },
+--                 comments = { italic = false, bold = false },
+--                 conditionals = { italic = false, bold = false },
+--                 delimiters = { italic = false, bold = false },
+--                 functions = { italic = false, bold = false },
+--                 keywords = { italic = false, bold = false },
+--                 loops = { italic = false, bold = false },
+--                 operators = { italic = false, bold = false },
+--                 properties = { italic = false, bold = false },
+--                 strings = { italic = false, bold = false },
+--                 types = { italic = false, bold = false },
+--                 variables = { italic = false, bold = false },
+--             },
+--         })
+--         vim.cmd("colorscheme horizon-extended")
+--     end,
+-- }
