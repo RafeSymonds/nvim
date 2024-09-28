399c039d9d489f36cafd61cffd0808d27d924cea
Rafe <rsymonds@umich.edu>
2024-09-28 15:24:02 -0400

    desktop

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,7 +7,7 @@ return {
         require("flow").setup({
             transparent = false, -- Set transparent background.
             fluo_color = "pink", --  Fluo color: pink, yellow, orange, or green.
-            mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+            mode = "bright", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
             aggressive_spell = false, -- Display colors for spell check.
         })
 
