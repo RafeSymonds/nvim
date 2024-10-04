3338ea8a76119bef67b0bc6310c04781ec4053b3
Rafe <rsymonds@umich.edu>
2024-10-04 16:22:18 -0400

    moved fonts plus copying fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,4 @@ require("telescope").load_extension("file_browser")
 -- Define the custom mapping
 vim.keymap.set("n", "<leader><space>", ":Telescope file_browser path=%:p:h=%:p:h<cr>", { desc = "Browse files" })
 vim.keymap.set("n", "<C-p>", '"_viwp', { desc = "Paste and delete", noremap = true, silent = true })
+-- vim.keymap.set("n", "<C-0>", '"_ddp', { desc = "Paste and delete", noremap = true, silent = true })
