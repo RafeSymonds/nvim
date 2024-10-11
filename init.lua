b48c97551cf8eabc7547af473d7c6c53c1bbec88
Rafe <rsymonds@umich.edu>
2024-10-10 22:31:54 -0400

    moved formatting to one file

--- init.lua
+++ init.lua
@@ -1,13 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- add custom formatting for vim
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.smartindent = true
-vim.opt.autoindent = true
-vim.opt.cindent = true
-
 -- change autocomplete to tab instead of enter
 local cmp = require("cmp")
 cmp.setup({
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,14 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_python_lsp = "pyright"
-
 vim.g.lazyvim_python_ruff = "ruff"
-
 vim.g.autoformat = true
 
-vim.o.softtabstop = 4
+-- add custom formatting for vim
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.cindent = true
 
-vim.o.shiftwidth = 4
-
-vim.o.title = true
-vim.o.titlestring = "%{fnamemodify(getcwd(), ':t')}"
+vim.opt.title = true
+vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}"
