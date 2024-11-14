47c02d470a528259e8deb5bfc953e86ab3bc1442
Rafe <rsymonds@umich.edu>
2024-11-14 09:53:42 -0500

    turned off indenter and got new one

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,36 +1,23 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    event = "BufReadPost",
-    opts = function()
-        -- Set highlight for indentation guides
-        vim.cmd([[highlight IndentBlanklineChar guifg=#111111 ctermfg=black]])
+    -- Disable the indent-blankline plugin
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        enabled = false, -- This line disables the plugin
+    },
 
-        return {
-            indent = {
-                smart_indent_cap = true,
-                priority = 2,
-                repeat_linebreak = false,
-            },
-            scope = {
-                show_start = false,
-                show_end = false,
-            },
-            exclude = {
-                filetypes = {
-                    "help",
-                    "alpha",
-                    "dashboard",
-                    "neo-tree",
-                    "Trouble",
-                    "trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "lazyterm",
-                },
-            },
-        }
-    end,
-    main = "ibl",
+    -- Install yggdroot/indentLine with custom settings
+    {
+        "yggdroot/indentLine",
+        event = "BufReadPost", -- Trigger plugin when a file is read
+        config = function()
+            -- Optional: Customization options (e.g., colors)
+            vim.g.indentLine_char = "|" -- Character for indentation lines
+            vim.g.indentLine_color_term = 235 -- Darker color (you can adjust this to your terminal theme)
+            vim.g.indentLine_guifg = "#3e3e3e" -- GUI color for darker line (hex color for GUI)
+
+            -- Set position of the indent line (most left)
+            vim.g.indentLine_setColors = 0 -- Disable additional colorization
+            vim.g.indentLine_fileTypeExclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" } -- Exclude certain file types
+        end,
+    },
 }
